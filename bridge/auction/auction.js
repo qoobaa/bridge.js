7a334fc0354c5d007975aca3d42c66ce3e6672a9
Jakub Kuźma <qoobaa@gmail.com>
2011-08-19 17:41:25 +0200

    added add in auction and tests

--- bridge/auction/auction.js
+++ bridge/auction/auction.js
@@ -8,11 +8,75 @@ YUI.add("bridge-auction", function (Y) {
         if (Y.instanceOf(arrayOrAuction, Auction)) {
             this._bids = arrayOrAuction._bids.slice(0);
         } else {
-            this._bids = Y.Array.map(arrayOrAuction, function (stringOrBid) {
-                return Y.Bridge.Bid(stringOrBid);
-            });
+            this._bids = [];
+            Y.Array.each(arrayOrAuction, function (stringOrBid, i) {
+                if (!this.add(stringOrBid)) {
+                    Y.error("invalid bid " + stringOrBid + " at position " + i);
+                }
+            }, this);
         }
     }
+
+    Auction.prototype = {
+
+        size: function () {
+            return this._bids.length;
+        },
+
+        contracts: function () {
+            return Y.Array.filter(this._bids, function (bid) {
+                return bid.isContract();
+            });
+        },
+
+        lastContract: function () {
+            var contracts = this.contracts();
+
+            return contracts[contracts.length - 1];
+        },
+
+        add: function (stringOrBid) {
+            var lastContract, bid1, bid2, bid3,
+                length = this._bids.length,
+                bid = Y.Bridge.Bid(stringOrBid);
+
+            bid1 = this._bids[length - 1];
+            bid2 = this._bids[length - 2];
+            bid3 = this._bids[length - 3];
+
+            if (this.isFinished()) {
+                return false;
+            } else if (bid.isDouble()) {
+                if (!((bid1 && bid1.isContract()) || (bid1 && bid1.isPass() && bid2 && bid2.isPass() && bid3 && bid3.isContract()))) {
+                    return false;
+                }
+            } else if (bid.isRedouble()) {
+                if (!((bid1 && bid1.isDouble()) || (bid1 && bid1.isPass && bid2 && bid2.isPass() && bid3 && bid3.isDouble()))) {
+                    return false;
+                }
+            } else if (bid.isContract()) {
+                lastContract = this.lastContract();
+
+                if (lastContract && lastContract.compareTo(bid) >= 0) {
+                    return false;
+                }
+            }
+
+            this._bids.push(bid);
+            return true;
+        },
+
+        isFinished: function () {
+            if (this._bids.length > 3) {
+                return Y.Array.every(this._bids.slice(-3), function (bid) {
+                    return bid.isPass();
+                });
+            } else {
+                return false;
+            }
+        }
+
+    };
 
     Y.namespace("Bridge").Auction = Auction;
 
--- test/auction/auction.js
+++ test/auction/auction.js
@@ -2,15 +2,21 @@ YUI.add("bridge-auction-test", function (Y) {
 
     var Auction = Y.Bridge.Auction,
         isTrue = Y.Assert.isTrue,
+        isFalse = Y.Assert.isFalse,
         areSame = Y.Assert.areSame;
 
     Y.namespace("Bridge.Test").Auction = new Y.Test.Case({
 
         name: "Auction Tests",
 
+        setUp: function () {
+            this.auction = new Auction();
+        },
+
         _should: {
             error: {
-                testConstructingWithInvalidString: true
+                testConstructingWithInvalidString: true,
+                testConstructingWithDouble: true
             }
         },
 
@@ -18,6 +24,84 @@ YUI.add("bridge-auction-test", function (Y) {
 
         testConstructingWithInvalidString: function () {
             new Auction(["HA"]);
+        },
+
+        testConstructingWithPass: function () {
+            new Auction(["PASS"]);
+        },
+
+        testConstructingWithDouble: function () {
+            new Auction(["X"]);
+        },
+
+        testConstructingWithEmptyArray: function () {
+            new Auction([]);
+        },
+
+        testConstructingWithoutArguments: function () {
+            new Auction();
+        },
+
+        // add
+
+        testAddWithPasses: function () {
+            isTrue(this.auction.add("PASS"));
+            isTrue(this.auction.add("PASS"));
+            isTrue(this.auction.add("PASS"));
+            isTrue(this.auction.add("PASS"));
+            isFalse(this.auction.add("PASS"));
+        },
+
+        testAddWithContracts: function () {
+            isTrue(this.auction.add("1C"));
+            isTrue(this.auction.add("1D"));
+            isTrue(this.auction.add("1H"));
+            isTrue(this.auction.add("1S"));
+            isTrue(this.auction.add("1NT"));
+        },
+
+        testAddReturnsFalseOnContractLowerThanLast: function () {
+            isTrue(this.auction.add("1NT"));
+            isFalse(this.auction.add("1S"));
+            isFalse(this.auction.add("1H"));
+            isFalse(this.auction.add("1D"));
+            isFalse(this.auction.add("1C"));
+        },
+
+        testAddWithDouble: function () {
+            isTrue(this.auction.add("1C"));
+            isTrue(this.auction.add("X"));
+            isFalse(this.auction.add("X"));
+        },
+
+        testAddWithDoubleAndPasses: function () {
+            isTrue(this.auction.add("1C"));
+            isTrue(this.auction.add("PASS"));
+            isTrue(this.auction.add("PASS"));
+            isTrue(this.auction.add("X"));
+        },
+
+        testAddWithDoubleWithoutContract: function () {
+            isTrue(this.auction.add("PASS"));
+            isTrue(this.auction.add("PASS"));
+            isTrue(this.auction.add("PASS"));
+            isFalse(this.auction.add("X"));
+        },
+
+        testAddWithRedouble: function () {
+            isTrue(this.auction.add("1C"));
+            isTrue(this.auction.add("X"));
+            isTrue(this.auction.add("XX"));
+            isFalse(this.auction.add("XX"));
+        },
+
+        testAddWithRedoubleAndPasses: function () {
+            isTrue(this.auction.add("1C"));
+            isTrue(this.auction.add("X"));
+            isTrue(this.auction.add("PASS"));
+            isTrue(this.auction.add("PASS"));
+            isTrue(this.auction.add("XX"));
+            isFalse(this.auction.add("XX"));
         }
 
     });
